187f7ba02c0e1387af07ee4d90455ae2e17bed92
Григорий Давыдовский <lefaur@yandex.ru>
2022-06-26 12:01:37 +0300

    review, comments 6.1

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -182,7 +182,7 @@ class ReviewViewSet(BaseReviewCommentViewSet):
         return get_object_or_404(Title, id=self.kwargs.get('title_id'))
 
     def get_queryset(self):
-        return self.get_title().reviews.all()#.order_by('-pub_date')
+        return self.get_title().reviews.all()
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user, title=self.get_title())
@@ -197,7 +197,7 @@ class CommentViewSet(BaseReviewCommentViewSet):
         return get_object_or_404(Review, id=self.kwargs.get('reviews'))
 
     def get_queryset(self):
-        return self.get_review().comments.all()#.order_by('-pub_date')
+        return self.get_review().comments.all()
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user, review=self.get_review())
